e49eb93b135b7a9d4d5d7db2eb3f5a8ce290dc94
Alexis Castillo <alexis.castillo.pimienta@gmail.com>
2022-05-28 18:56:41 -0600

    Se comienza a trabajar CRUD

---
+++ js/urls.js
@@ -0,0 +1,306 @@
+// Colocar el URL despues de 'https://www.youtube.com/embed/'
+ytT1 = [
+    {
+        "Id": 0,
+        "Nombre": 'Piloto',
+        "URL": 'JZ-d--REspY'
+    },
+    {
+        "Id": 1,
+        "Nombre": 'IngenieriaDeProcesos',
+        "URL": 'RfnbzdJrPgc'
+    },
+    {
+        "Id": 2,
+        "Nombre": 'El Proceso de Salir de tu Zona de Confort',
+        "URL": 'Udfs4OXb4M4'
+    },
+    {
+        "Id": 3,
+        "Nombre": '¿Profesionales de la Comunicación e Ing. de Procesos?',
+        "URL": 'uLyoenmoWKI'
+    },
+    {
+        "Id": 4,
+        "Nombre": 'DMAIC - Definir',
+        "URL": 'AUltdlUloPo'
+    },
+    {
+        "Id": 5,
+        "Nombre": 'DMAIC - Medir',
+        "URL": 'KkUKCTg6590'
+    },
+    {
+        "Id": 6,
+        "Nombre": 'DMAIC - Analizar',
+        "URL": 's72f0bTgouo'
+    },
+    {
+        "Id": 7,
+        "Nombre": 'DMAIC - Mejorar',
+        "URL": 'GiU5Tx8s2dg'
+    },
+    {
+        "Id": 8,
+        "Nombre": 'DMAIC - Control',
+        "URL": 'y5L1sWClsec'
+    },
+    {
+        "Id": 9,
+        "Nombre": 'Hablemos de Industria 4.0',
+        "URL": 'Mx7LSLZC0qk'
+    },
+    {
+        "Id": 10,
+        "Nombre": "Indicadores de Desempeño (KPI's)",
+        "URL": '3bdew0jLHEw'
+    },
+    {
+        "Id": 11,
+        "Nombre": '¿El Límite de la Mejora Continua? (Costo-Tiempo-Calidad)',
+        "URL": 'bJ9dW-s9PSU'
+    },
+    {
+        "Id": 12,
+        "Nombre": 'Finanzas Personales / Empresariales y Mejora Continua',
+        "URL": 'JsFhfaKB_6I'
+    },
+    {
+        "Id": 13,
+        "Nombre": 'Liderazgo y Mejora Continua',
+        "URL": '5zA9lq91mNM'
+    },
+    {
+        "Id": 14,
+        "Nombre": 'Logística y Cadena de Suministro',
+        "URL": 'huPfuJ-3J2k'
+    },
+    {
+        "Id": 15,
+        "Nombre": 'Las 7 Herramientas Básicas de Calidad',
+        "URL": 'SJ31aTY0GKI'
+    },
+    {
+        "Id": 16,
+        "Nombre": 'Administración de Inventario',
+        "URL": 'elLW0BI46b0'
+    },
+    {
+        "Id": 17,
+        "Nombre": 'Tips para Conseguir tu Primer Empleo',
+        "URL": 'QOcIp0N-9Fg'
+    },
+    {
+        "Id": 18,
+        "Nombre": 'Los 8 Desperdicios',
+        "URL": '5ykid9e1kkU'
+    },
+    {
+        "Id": 19,
+        "Nombre": 'Inteligencia Artificial',
+        "URL": 'TY5pGElsob0'
+    },
+    {
+        "Id": 20,
+        "Nombre": '4 Libros de Mejora Continua',
+        "URL": 'aYocXZNo5Pw'
+    }
+];
+ytT2 = [
+    {
+        "Id": 1,
+        "Nombre": 'Capital Humano: Tendencias y Mejora Continua',
+        "URL": 'e5Dc6mhKUFo',
+    },
+    {
+        "Id": 2,
+        "Nombre": "Estudiar o No Una Maestría",
+        "URL": 'qEMkX5ieaB8',
+    },
+    {
+        "Id": 3,
+        "Nombre": 'Control de Almacén Vs Control de Inventario',
+        "URL": '7yP9Z-8GKNY',
+    },
+    {
+        "Id": 4,
+        "Nombre": 'Todos Debemos de Aprender a Programar',
+        "URL": 'EU2PmVilh0k'
+    },
+    {
+        "Id": 5,
+        "Nombre": 'Logística y Cadena de Suministro con Lucy González',
+        "URL": 'ouWZPZYutAQ',
+    },
+    {
+        "Id": 6,
+        "Nombre": 'Hablemos de RPA',
+        "URL": '1hCjostIoJU'
+    },
+    {
+        "Id": 7,
+        "Nombre": 'Hablemos de Blockchain',
+        "URL": 'H-LbygPnOFM',
+    }
+];
+
+// Colocar el URL despues de 'https://open.spotify.com/embed/episode/'
+// y antes de '?utm_source=generator&theme=0'
+sp = [
+    {
+        "Id": 'T1E0',
+        "Nombre": 'Piloto',
+        "URL": '2KKK6W4bslPNveAzIAUetz',
+    },
+    {
+        "Id": 'T1E1',
+        "Nombre": 'Ingeniería de Procesos',
+        "URL": '5uc2DQxvxS7NfsDNrB7B8i',
+    },
+    {
+        "Id": 'T1E2',
+        "Nombre": 'El Proceso de Salir de tu Zona de Confort',
+        "URL": '5c3stOhy7C1I4cRfHlp87A',
+    },
+    {
+        "Id": 'T1E3',
+        "Nombre": '¿Profesionales de la Comunicación e Ing. de Procesos?',
+        "URL": '3QOfloYy2HaAnfu9Qsz5yP',
+    },
+    {
+        "Id": 'T1E4',
+        "Nombre": 'DMAIC - Definir',
+        "URL": '6yb7xDvITTbmWxedc3fhIp',
+    },
+    {
+        "Id": 'T1E5',
+        "Nombre": 'DMAIC - Medir',
+        "URL": '6rLRCZQO7YGa9ICwZusWuX',
+    },
+    {
+        "Id": 'T1E6',
+        "Nombre": 'DMAIC - Analizar',
+        "URL": '2DYHddXendx8VAkl44uwSl',
+    },
+    {
+        "Id": 'T1E7',
+        "Nombre": 'DMAIC - Mejorar',
+        "URL": '4cGCBiWUiOf13XuXKqhX8w',
+    },
+    {
+        "Id": 'T1E8',
+        "Nombre": 'DMAIC - Control',
+        "URL": '762QwSu7iKBaonpMfaKvrv',
+    },
+    {
+        "Id": 'T1E9',
+        "Nombre": 'Hablemos de Industria 4.0',
+        "URL": '0o45QMpGIfAzPSNkcYdQgR',
+    },
+    {
+        "Id": 'T1E10',
+        "Nombre": "Indicadores de Desempeño (KPI's)",
+        "URL": '7nDZwdHthb16OxOdv2IPnF',
+    },
+    {
+        "Id": 'T1E11',
+        "Nombre": '¿El Límite de la Mejora Continua? (Costo-Tiempo-Calidad)',
+        "URL": '6fRfEVzAiCS19k0dPYdycc',
+    },
+    {
+        "Id": 'T1E12',
+        "Nombre": 'Finanzas Personales / Empresariales y Mejora Continua',
+        "URL": '53B9oWi1I2e8SWZU43wVMn',
+    },
+    {
+        "Id": 'T1E13',
+        "Nombre": 'Liderazgo y Mejora Continua',
+        "URL": '0oazHzczozqfhTcJXg5Uzu',
+    },
+    {
+        "Id": 'T1E14',
+        "Nombre": 'Logística y Cadena de Suministro',
+        "URL": '1xojP5rccThncZsg8CD0wX',
+    },
+    {
+        "Id": 'T1E15',
+        "Nombre": 'Las 7 Herramientas Básicas de Calidad',
+        "URL": '1PQWmWBmdTfvZnm6HmgvN0',
+    },
+    {
+        "Id": 'T1E16',
+        "Nombre": 'Administración de Inventario',
+        "URL": '4HnDukFmo9gfQ4HRJmpYtE',
+    },
+    {
+        "Id": 'T1E17',
+        "Nombre": 'Tips para Conseguir tu Primer Empleo',
+        "URL": '3R0JbDbk5ARnplM3qnozUB',
+    },
+    {
+        "Id": 'T1E18',
+        "Nombre": 'Los 8 Desperdicios',
+        "URL": '3GrjjTRbEJbxPkc7B8NST3',
+    },
+    {
+        "Id": 'T1E19',
+        "Nombre": 'Inteligencia Artificial',
+        "URL": '3bcMJIqZ9BMaRQP3riz0Lo',
+    },
+    {
+        "Id": 'T1E20',
+        "Nombre": '4 Libros de Mejora Continua',
+        "URL": '0syZgCLlIhBCJVHqGP2gPS',
+    },
+    {
+        "Id": 'T2E1',
+        "Nombre": 'Capital Humano: Tendencias y Mejora Continua',
+        "URL": '6GrLVySyv4fNb0291spTHP',
+    },
+    {
+        "Id": 'T2E2',
+        "Nombre": "Estudiar o No Una Maestría",
+        "URL": '1urLlPxHbT0F7ltTFpk1BN',
+    },
+    {
+        "Id": 'T2E3',
+        "Nombre": 'Control de Almacén Vs Control de Inventario',
+        "URL": '5S2ltNltHx7mvPDimJvuLp',
+    },
+    {
+        "Id": 'T2E4',
+        "Nombre": 'Todos Debemos de Aprender a Programar',
+        "URL": '6AMIkDTI5I82iYVVu3cRyA'
+    },
+    {
+        "Id": 'T2E5',
+        "Nombre": 'Logística y Cadena de Suministro con Lucy González',
+        "URL": '4rsPCAa3qQY1Vfs6TNgpKz',
+    },
+    {
+        "Id": 'T2E6',
+        "Nombre": 'Hablemos de RPA',
+        "URL": '2yJjYWBL8slc0ivrivG4ni'
+    },
+    {
+        "Id": 'T2E7',
+        "Nombre": 'Hablemos de Blockchain',
+        "URL": '4d1KC18N58lwmO2jYuWOCc',
+    },
+    {
+        "Id": 'T2E8',
+        "Nombre": 'La Adopción de Modelos Híbridos y la Importancia del “Lado Humano” del Empleo',
+        "URL": '2oiFtjtITIXRoJSi7OaHDZ',
+    }
+];
+
+// Replaces 
+function repSp(Id) {
+    const auxObj = sp.filter(episodio => episodio.Id == Id);
+    const regEx = /(?<=episode\/).*?(?=\?utm)/g;
+    document.body.innerHTML = document.body.innerHTML.replace(regEx, auxObj[0].URL);
+    // console.log(auxObj[0].URL);
+}
+
+
+// Instagram followers
